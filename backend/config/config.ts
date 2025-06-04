2eef369b99176712357f2e4c5eb2dca652d32751
khadervali.marketing <khadervali.marketing@gmail.com>
2025-06-04 20:26:43 +0000

    commit message

---
+++ backend/config/config.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { Options, Dialect } from 'sequelize';
+
+interface DbConfig {
+  development: Options;
+  test?: Options;
+  production?: Options;
+}
+
+const config: DbConfig = {
+  development: {
+    dialect: process.env.DB_DIALECT as Dialect || 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : undefined,
+    database: process.env.DB_NAME || 'database_development',
+  },
+};
+
+export = config;
