1fef8933e4b827839ffb3f5124b6d9a02fe53b5c
khadervali4u <khadervali4u@gmail.com>
2025-06-10 21:33:18 +0000

    auth Middleware

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -14,13 +14,13 @@ export const protect = async (req: CustomRequest, res: Response, next: NextFunct
     const token = req.headers.authorization?.split(' ')[1];
     
     if (!token) {
-      return res.status(401).json({ message: 'Not authorized, no token' });
+      res.status(401).json({ message: 'Not authorized, no token' });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
     req.user = {
       id: decoded.user.id,
-      role: decoded.user.role,
+      role: decoded.user.role, // Changed from vendorId to vendor_id
       vendor_id: decoded.user.vendor_id // Changed from vendorId to vendor_id
     };
 
